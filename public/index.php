388ceae77538535a12857683ad305546c913c272
Diego Staniescki <Diego.stk@live.com>
2022-04-05 05:47:36 -0300

    Inclusao de template e adicionado estilos as paginas

--- public/index.php
+++ public/index.php
@@ -1,18 +1,14 @@
 <?php
-
 require_once(dirname(__FILE__,2). '/src/config/config.php');
-//require_once(VIEW_PATH . '/login.php');
 
-require_once (MODEL_PATH . '/Login.php');
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
 
-$login = new Login([
-    'email' => 'admin@cod3r.com.br',
-    'password' => 'a'
-]);
-
-try {
-    $login->checkLogin();
-    echo "Deu certo :)";
-}catch(Exception $e){
-    echo "Porblema no Login :P";
+if ($uri === '/' || $uri === '' || $uri === '/index.php'){
+    $uri = '/login.php';
 }
+
+require_once (CONTROLLER_PATH . "/{$uri}");
+
+
--- src/models/login.php
+++ src/models/login.php
@@ -1,16 +1,37 @@
 <?php
-require_once (realpath(MODEL_PATH . '/User.php'));
+LoadModel('User');
 
 class Login extends Model
 {
+
+    public function validate(){
+        $errors = [];
+        if (!$this->email){
+            $errors['email'] = 'Email é um campo Obrigatorio';
+        }
+
+        if (!$this->password){
+            $errors['password'] = 'Por favor, Informe a Senha';
+        }
+
+        if (count($errors) > 0){
+            throw new ValidationException($errors);
+        }
+    }
+
+
     public function checkLogin(){
+        $this->validate();
         $user = User::getOne(['email' => $this->email]);
         if ($user){
+            if($user->end_date){
+                throw new AppException('Usuario esta desligado da empresa.');
+            }
             if (password_verify($this->password, $user->password)){
                 return $user;
             }
         }
-        throw new Exception();
+        throw new AppException('Usuario/Senha inválidos');
     }
 
 }
